5ae01c0434d01c98dd8da5312d20cb1e4b69ecde
Efim <efempolites@gmail.com>
2025-06-10 23:41:40 +0300

    release v1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // rewrites: () => {
-  //   return [
-  //     {
-  //       source: '/auth/google',
-  //       destination: 'https://quick-online.ru/api/auth/google',
-  //     },
-  //     {
-  //       source: '/auth/github',
-  //       destination: 'https://quick-online.ru/api/auth/github',
-  //     },
-  //   ];
-  // },
   rewrites: () => {
     return [
       {
         source: '/auth/google',
-        destination: 'http://localhost:4200/auth/google',
+        destination: 'https://quick-online.ru/api/auth/google',
       },
       {
         source: '/auth/github',
-        destination: 'http://localhost:4200/auth/github',
+        destination: 'https://quick-online.ru/api/auth/github',
       },
     ];
   },
+  // rewrites: () => {
+  //   return [
+  //     {
+  //       source: '/auth/google',
+  //       destination: 'http://localhost:4200/auth/google',
+  //     },
+  //     {
+  //       source: '/auth/github',
+  //       destination: 'http://localhost:4200/auth/github',
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
